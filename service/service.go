14326c994578268b928f567ed7bccd75081ac255
Santiago Hernández <santiagohernandez.1997@gmail.com>
2025-05-30 01:07:17 -0300

    tests and stuff

--- service/service.go
+++ service/service.go
@@ -25,6 +25,12 @@ func New(log *logrus.Logger, target string, verbose bool, statusCode int, wait t
 		log.WithField("target", target).Debug("Forwarding requests to target")
 	}
 
+	if statusCode < 100 || statusCode > 999 {
+		if statusCode != 0 {
+			log.WithField("status_code", statusCode).Warn("Invalid status code, using 200 as fallback")
+		}
+		statusCode = http.StatusOK
+	}
 	return &service{
 		log:        log,
 		target:     target,
@@ -37,50 +43,52 @@ func New(log *logrus.Logger, target string, verbose bool, statusCode int, wait t
 func (s *service) Handler(w http.ResponseWriter, r *http.Request) {
 	s.logRequestDetails(r)
 
-	if s.target != "" {
-		uri, err := url.JoinPath(s.target, r.RequestURI)
-		if err != nil {
-			s.log.WithError(err).
-				WithField("target", s.target).
-				WithField("path", r.URL.Path).
-				Error("Error joining paths")
-			return
-		}
-
-		res, err := forwardRequest(r, uri)
-		if err != nil {
-			s.log.WithError(err).WithField("target", s.target).Error("Error forwarding request")
-			w.WriteHeader(http.StatusBadGateway)
-			return
-		}
-
-		for k, v := range res.Header {
-			for _, vv := range v {
-				w.Header().Add(k, vv)
-			}
-		}
-		w.WriteHeader(res.StatusCode)
-
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			s.log.WithError(err).Error("Error reading response body")
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		defer res.Body.Close()
+	if s.target == "" {
+		// No target configured, sleep and respond.
 		time.Sleep(s.wait)
-		if _, err := w.Write(body); err != nil {
-			s.log.WithError(err).Error("Error writing response body")
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+		if s.statusCode != 200 {
+			w.WriteHeader(s.statusCode)
 		}
 		return
 	}
 
+	uri, err := url.JoinPath(s.target, r.RequestURI)
+	if err != nil {
+		s.log.WithError(err).
+			WithField("target", s.target).
+			WithField("path", r.URL.Path).
+			Error("Error joining paths")
+		return
+	}
+
+	res, err := forwardRequest(r, uri)
+	if err != nil {
+		s.log.WithError(err).WithField("target", s.target).Error("Error forwarding request")
+		time.Sleep(s.wait)
+		w.WriteHeader(http.StatusBadGateway)
+		return
+	}
+
+	for k, v := range res.Header {
+		for _, vv := range v {
+			w.Header().Add(k, vv)
+		}
+	}
+	w.WriteHeader(res.StatusCode)
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		s.log.WithError(err).Error("Error reading response body")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	defer res.Body.Close()
 	time.Sleep(s.wait)
-	if s.statusCode != 200 {
-		w.WriteHeader(s.statusCode)
+	if _, err := w.Write(body); err != nil {
+		s.log.WithError(err).Error("Error writing response body")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 }
 
@@ -95,28 +103,22 @@ func (s *service) logRequestDetails(r *http.Request) {
 		headers = append(headers, fmt.Sprintf("%s: %s", k, strings.Join(v, ", ")))
 	}
 
-	switch r.Method {
-	case http.MethodPatch, http.MethodPost, http.MethodPut:
+	fields := logrus.Fields{
+		"method":  r.Method,
+		"uri":     r.RequestURI,
+		"headers": strings.Join(headers, ", "),
+	}
+
+	if r.Method == http.MethodPatch || r.Method == http.MethodPost || r.Method == http.MethodPut {
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
 			s.log.WithError(err).Error("Error reading request body")
 			return
 		}
 		r.Body = io.NopCloser(bytes.NewBuffer(body))
-		s.log.WithFields(
-			logrus.Fields{
-				"method":  r.Method,
-				"uri":     r.RequestURI,
-				"headers": strings.Join(headers, ", "),
-				"body":    string(body),
-			}).Info("Request received")
-	default:
-		s.log.WithFields(logrus.Fields{
-			"method":  r.Method,
-			"uri":     r.RequestURI,
-			"headers": strings.Join(headers, ", "),
-		}).Info("Request received")
+		fields["body"] = string(body)
 	}
+	s.log.WithFields(fields).Info("Request received")
 }
 
 func forwardRequest(r *http.Request, target string) (*http.Response, error) {
--- service/service_test.go
+++ service/service_test.go
@@ -1,67 +1,167 @@
 package service_test
 
 import (
-	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/santihernandezc/devnull/service"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
 )
 
-func TestForwardRequest(t *testing.T) {
-	targetSrv := httptest.NewServer(http.HandlerFunc(handler))
+func TestHandler(t *testing.T) {
+	targetServerHeaders := map[string][]string{
+		"Target-Header-1": []string{"test-1"},
+		"Target-Header-2": []string{"test-2"},
+	}
+	targetServerResponse := []byte("this is a test response")
+	ft := newFakeTarget(targetServerResponse, targetServerHeaders)
+	targetSrv := httptest.NewServer(http.HandlerFunc(ft.handler))
 	defer targetSrv.Close()
 
 	tests := []struct {
 		name          string
-		target        string
+		headers       map[string][]string
 		statusCode    int
+		target        string
 		wait          time.Duration
 		expStatusCode int
+		expHeaders    map[string][]string
+		expResponse   []byte
 	}{
+		{
+			name:          "no target, default status code",
+			expStatusCode: http.StatusOK,
+		},
 		{
 			name:          "no target, custom status code",
 			statusCode:    http.StatusNotAcceptable,
 			expStatusCode: http.StatusNotAcceptable,
 		},
+		{
+			name:          "no target, custom wait time",
+			expStatusCode: http.StatusOK,
+			wait:          time.Second,
+		},
+		{
+			name:          "no target, bad status code",
+			statusCode:    19,
+			expStatusCode: http.StatusOK,
+			wait:          time.Second,
+		},
+		{
+			name:          "bad target, custom wait time",
+			target:        "http:bad",
+			expStatusCode: http.StatusBadGateway,
+			wait:          time.Second,
+		},
+		{
+			name:          "target configured, custom wait time",
+			target:        targetSrv.URL,
+			expStatusCode: http.StatusTeapot,
+			wait:          time.Second,
+			expHeaders:    targetServerHeaders,
+			expResponse:   targetServerResponse,
+		},
+		{
+			name:          "target configured, custom status code ignored",
+			target:        targetSrv.URL,
+			statusCode:    http.StatusAccepted,
+			expStatusCode: http.StatusTeapot,
+			expHeaders:    targetServerHeaders,
+			expResponse:   targetServerResponse,
+		},
+		{
+			name:          "target configured, custom headers",
+			target:        targetSrv.URL,
+			headers:       map[string][]string{"Test-Header-1": []string{"test-1"}, "Test-Header-2": []string{"test-2"}},
+			expStatusCode: http.StatusTeapot,
+			expHeaders:    targetServerHeaders,
+			expResponse:   targetServerResponse,
+		},
 	}
 
-	l := log.New(io.Discard, "", 0)
-
+	l := logrus.New()
+	l.SetOutput(io.Discard)
 	for _, test := range tests {
-		svc := service.New(l, test.target, false, test.statusCode, test.wait)
-		s := httptest.NewServer(http.HandlerFunc(svc.Handler))
-		defer s.Close()
+		t.Run(test.name, func(t *testing.T) {
+			t.Cleanup(ft.cleanup)
+			svc := service.New(l, test.target, false, test.statusCode, test.wait)
+			s := httptest.NewServer(http.HandlerFunc(svc.Handler))
+			defer s.Close()
 
-		res, err := http.Get(s.URL)
-		if err != nil {
-			t.Fatalf("Error making GET request: %v", err)
-		}
+			now := time.Now()
+			req, err := http.NewRequest(http.MethodGet, s.URL+"/test", nil)
+			require.NoError(t, err)
 
-		defer func() {
-			if err := res.Body.Close(); err != nil {
-				t.Fatalf("Error closing request body: %v", err)
+			for k, v := range test.headers {
+				for _, vv := range v {
+					req.Header.Add(k, vv)
+				}
 			}
-		}()
 
-		if res.StatusCode != test.expStatusCode {
-			t.Fatalf("Unexpected status code, got=%d, want=%d", res.StatusCode, test.expStatusCode)
-		}
+			res, err := http.DefaultClient.Do(req)
+			require.NoError(t, err)
+			require.Equal(t, test.expStatusCode, res.StatusCode)
 
-		b, err := io.ReadAll(res.Body)
-		if err != nil {
-			t.Fatalf("Error reading respone body: %v", err)
-		}
+			elapsed := time.Since(now)
+			require.Greater(t, elapsed, test.wait)
 
-		fmt.Println(string(b))
+			for k, v := range test.headers {
+				header := ft.receivedHeaders[k]
+				for _, vv := range v {
+					require.Contains(t, header, vv)
+				}
+			}
+
+			for k, v := range test.expHeaders {
+				header := res.Header[k]
+				for _, vv := range v {
+					require.Contains(t, header, vv)
+				}
+			}
+
+			if len(test.expResponse) > 0 {
+				defer func() {
+					require.NoError(t, res.Body.Close())
+				}()
+
+				b, err := io.ReadAll(res.Body)
+				require.NoError(t, err)
+				require.Equal(t, test.expResponse, b)
+			}
+		})
 	}
-
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+type fakeTarget struct {
+	receivedHeaders map[string][]string
+	response        []byte
+	responseHeaders map[string][]string
+}
+
+func newFakeTarget(response []byte, headers map[string][]string) *fakeTarget {
+	return &fakeTarget{
+		receivedHeaders: make(map[string][]string),
+		response:        response,
+		responseHeaders: headers,
+	}
+}
+
+func (f *fakeTarget) handler(w http.ResponseWriter, r *http.Request) {
+	for k, v := range f.responseHeaders {
+		for _, vv := range v {
+			w.Header().Add(k, vv)
+		}
+	}
 	w.WriteHeader(http.StatusTeapot)
+	f.receivedHeaders = r.Header
+	w.Write(f.response)
+}
+
+func (f *fakeTarget) cleanup() {
+	f.receivedHeaders = make(map[string][]string)
 }
